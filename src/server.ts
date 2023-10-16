1dba75ce79df8e82f46727fe37a90dd229ff876f
summer <80736033+yl9517@users.noreply.github.com>
2023-10-16 11:06:53 +0900

    method

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ app.get("/", (req: Request, res: Response) => {
   res.send(`Hello World! It's version port : ${PORT}`);
 });
 
-app.get("/abc324", (req: Request, res: Response) => {
+app.get("/abc3224", (req: Request, res: Response) => {
   res.send(`abc comment+`);
 });
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
