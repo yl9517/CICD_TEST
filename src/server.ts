84094e3a69cd4f22b0b06391397818345c00ccc9
summer <80736033+yl9517@users.noreply.github.com>
2023-10-16 11:29:52 +0900

    fix

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ app.get("/", (req: Request, res: Response) => {
   res.send(`Hello World! It's version port : ${PORT}`);
 });
 
-app.get("/abc32s4", (req: Request, res: Response) => {
+app.get("/abc32s42", (req: Request, res: Response) => {
   res.send(`abc comment+`);
 });
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
